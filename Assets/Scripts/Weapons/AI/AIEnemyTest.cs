eeaecd28c4a23adef8d620ec0ae2fa07293a460b
JP <jphil.provost@outlook.com>
2026-03-23 13:10:08 -0400

    Enemy Art

---
+++ Assets/Scripts/Weapons/AI/AIEnemyTest.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AIEnemyTest : AIEnemyBase
+{
+    [SerializeField] private AIEnemyTestStates currentState;
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private HearingBehaviour hearingBehaviour;
+    [SerializeField] private Animator animator;
+
+    private void Update()
+    {
+        UpdateState();
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+
+        float speed = navMeshAgent.velocity.sqrMagnitude;
+
+        float normalizedSpeed = Mathf.Lerp(-1, 1f, speed);
+
+        animator.SetFloat("forwardVelocity", normalizedSpeed);
+        animator.SetFloat("horizontalVelocity", 0);        
+    }
+
+
+    //Update State 
+    private void UpdateState()
+    {
+        switch (currentState)
+        {
+            case AIEnemyTestStates.IDLE:
+                IdleBehaviour();
+                break;
+
+            case AIEnemyTestStates.CHASE:
+                ChaseBehaviour();
+                break;
+
+            case AIEnemyTestStates.PATROL:
+                PatrolBehaviour();
+                break;
+
+            case AIEnemyTestStates.ATTACK:
+                AttackBehaviour();
+                break;
+
+            case AIEnemyTestStates.DIE:
+                DieBehaviour();
+                break;
+        }
+    }
+
+    //Enter state
+    private void SetState(AIEnemyTestStates newState)
+    {
+        if (currentState == AIEnemyTestStates.DIE)
+            return;
+
+        switch (newState)
+        {
+            case AIEnemyTestStates.IDLE:
+                animator.SetBool("Idle", true);
+                hearingBehaviour.onHeardPlayer += OnHeardPlayer;
+                break;
+
+            case AIEnemyTestStates.CHASE:
+                break;
+
+            case AIEnemyTestStates.PATROL:
+                break;
+
+            case AIEnemyTestStates.ATTACK:
+                break;
+        }
+
+        currentState = newState;
+
+    }
+
+    private void OnHeardPlayer()
+    {
+        SetState(AIEnemyTestStates.CHASE);
+        hearingBehaviour.onHeardPlayer -= OnHeardPlayer;
+    }
+
+
+    #region BEHAVIOURS
+
+    private void IdleBehaviour()
+    {
+     
+    }
+
+    private void PatrolBehaviour()
+    {
+
+    }
+
+    private void AttackBehaviour()
+    {
+
+    }
+
+    private void ChaseBehaviour()
+    {
+
+    }
+
+    private void DieBehaviour()
+    {
+
+    }
+
+    #endregion
+
+    private void OnDestroy()
+    {
+        hearingBehaviour.onHeardPlayer -= OnHeardPlayer;
+    }
+
+
+    private enum AIEnemyTestStates
+    {
+        IDLE,
+        PATROL,
+        CHASE,
+        ATTACK,
+        DIE
+    }
+}
---
+++ Assets/Scripts/Weapons/AI/HearingBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class HearingBehaviour : MonoBehaviour
+{
+    [SerializeField] private float hearingRange = 5;
+
+    private bool canHearPlayer = false;
+
+    public bool CanHearPlayer => canHearPlayer;
+    public event Action onHeardPlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            canHearPlayer = true;
+            onHeardPlayer?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canHearPlayer = false;
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(this.transform.position, hearingRange);
+    }
+
+}
